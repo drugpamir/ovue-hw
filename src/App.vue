cd882ed99fec9604be8127914e50d56414b60ce6
drugpamir <toll_aa@mail.ru>
2025-11-15 23:34:33 +0300

    removed some other default vue styles and components (views)

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,7 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      Hello Vue
-    </div>
-  </header>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    //place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    //place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
