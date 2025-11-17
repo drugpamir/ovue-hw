d187d22f5a710c43857736171b2fbed2c9d1d441
drugpamir <toll_aa@mail.ru>
2025-11-18 00:14:11 +0300

    USERS array moved from component to own store file

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,28 +1,7 @@
 <script setup>
 import User from "@/components/User.vue";
-// import {USERS} from "@/components/UserStore.vue";
+import {USERS} from "@/components/UserStore.js";
 import {ref} from "vue";
-
-const USERS = ref([
-  {
-    id: 1,
-    firstName: 'Вася',
-    lastName: 'Иванов',
-    birthDate: new Date(1990, 3, 1)
-  },
-  {
-    id: 2,
-    firstName: 'Дмитрий',
-    lastName: 'Петров',
-    birthDate: new Date(1999, 2, 13)
-  },
-  {
-    id: 3,
-    firstName: 'Кузнецова',
-    lastName: 'Наталья',
-    birthDate: new Date(1995, 5, 21)
-  },
-])
 
 const userListIsVisible = ref(true)
 const toggleUserList = () => userListIsVisible.value = !userListIsVisible.value
