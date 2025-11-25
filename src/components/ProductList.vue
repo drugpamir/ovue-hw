5d0ed3db0dc8d97f65d70548d73b728e00b5150a
drugpamir <toll_aa@mail.ru>
2025-11-26 02:01:22 +0300

    search input dummy for products filtering at the first...

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,18 +1,29 @@
 <script setup>
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import Product from "@/components/Product.vue";
+import SearchProduct from "@/components/SearchProduct.vue";
 import {error, fetchProducts, loading, products} from "@/components/ProductService.js";
 
 onMounted(fetchProducts)
+
+const filteredProducts = ref(products.value)
+
+function onSearchProduct(productTitle, productMinPrice, productMaxPrice) {
+  filteredProducts.value = products.value.filter((p) =>
+      (!productTitle.value.length || p.value === productTitle.value)
+      && p.value > productMinPrice.value
+      && p.value < productMaxPrice.value)
+}
 </script>
 
 <template>
-  <p v-if="loading">Загрузка продуктов...</p>
-  <p v-if="error">Ошибка: {{ error }}</p>
-  <div v-if="products.length" class="products-grid">
-    <Product v-for="product in products" :key="product.id" v-bind="product"/>
-  </div>
-  <p v-else>Список продуктов пуст!</p>
+  <SearchProduct @searchProduct="onSearchProduct"/>
+    <p v-if="loading">Загрузка продуктов...</p>
+    <p v-if="error">Ошибка: {{ error }}</p>
+    <div v-if="products.length" class="products-grid">
+      <Product v-for="product in products" :key="product.id" v-bind="product"/>
+    </div>
+    <p v-else>Список продуктов пуст!</p>
 </template>
 
 <style scoped>
---
+++ src/components/SearchProduct.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref} from "vue";
+
+const productTitle = ref('')
+const productMinPrice = ref(0)
+const productMaxPrice = ref(1E100)
+
+const emit = defineEmits(['onFilterChange'])
+
+function searchProduct(productTitle, productMinPrice, productMaxPrice) {
+  emit("onFilterChange", productTitle, productMinPrice, productMaxPrice);
+}
+</script>
+
+<template>
+  <input type="search" v-model="productTitle" @change="searchProduct" name="title" id="product-title-input"
+         placeholder="Product name to search">
+  <input type="number" v-model="productMinPrice" @change="searchProduct" name="min-price" id="product-min-price-input">
+  <input type="number" v-model="productMaxPrice" @change="searchProduct" name="max-price" id="product-max-price-input">
+</template>
+
+<style scoped>
+
+</style>
